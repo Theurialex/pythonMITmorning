e2779a96e180aae43f74ad9ea1035663e3ed88a8
Theurialex <theurialex75@gmail.com>
2023-06-29 10:37:05 +0300

    initial commit

--- class.py
+++ class.py
@@ -1,9 +1,16 @@
 class fruits:
-    def __init__(self,type,color,price):
+    def __init__(self,type,color,price,shape):
         self.fruittype=type
         self.fruitcolor=color
         self.fruitprice=price
+        self.fruitshape=shape
     def display(self):
-        print(self.fruittype,self.fruitcolor,self.fruitprice)
-myobj=fruits("banana","green",20)
+        print(self.fruittype,self.fruitcolor,self.fruitprice,self.fruitshape)
+myobj=fruits("banana","green",20,"oval")
 myobj.display()
+myobj1=fruits("mango","yellow",30,"oval")
+myobj1.display()
+myobj2=fruits("orange","orange",15,"sphere")
+myobj2.display()
+
+#class called peaple with name age and gender
